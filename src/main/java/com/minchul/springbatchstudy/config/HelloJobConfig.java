75db18fc6efef1d398541aede8a558d7bb6e2df1
MinChul-Son <smc5236@gmail.com>
2022-03-25 18:38:18 +0900

    ✨ feat: 간단한 Job과 Step 생성

---
+++ src/main/java/com/minchul/springbatchstudy/config/HelloJobConfig.java
@@ -0,0 +1,46 @@
+package com.minchul.springbatchstudy.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class HelloJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                                .start(helloStep1())
+                                .next(helloStep2())
+                                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return stepBuilderFactory.get("helloStep1")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step1");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step2");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+}
