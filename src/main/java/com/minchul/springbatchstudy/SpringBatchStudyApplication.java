d1bc077c72392ccabde0923cd9143b59502d6d4a
MinChul-Son <smc5236@gmail.com>
2022-03-25 17:08:25 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/minchul/springbatchstudy/SpringBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.minchul.springbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minchul/springbatchstudy/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minchul.springbatchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
