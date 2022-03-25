19c0124326456982a1d58beccf30dde360f1b26c
MinChul-Son <smc5236@gmail.com>
2022-03-25 18:37:57 +0900

    ✨ feat: 애노테이션 추가 및 DataSource 제외하도록 속성 추가
    - DB 설정을 아직 하지 않아서 AutoConfiguration 제외함
    - Spring Batch에는 Spring JDBC가 종속되어 있기 때문에 DB 설정하지 않으면 에러남

--- src/main/java/com/minchul/springbatchstudy/SpringBatchStudyApplication.java
+++ src/main/java/com/minchul/springbatchstudy/SpringBatchStudyApplication.java
@@ -1,9 +1,12 @@
 package com.minchul.springbatchstudy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableBatchProcessing
 public class SpringBatchStudyApplication {
 
 	public static void main(String[] args) {
