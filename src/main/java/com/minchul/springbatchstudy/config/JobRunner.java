a1257336c6944500ab32c9996dd5678d459e6644
MinChul-Son <smc5236@gmail.com>
2022-04-05 21:29:17 +0900

    ✨ feat: 다양한 JobParameter 추가

--- src/main/java/com/minchul/springbatchstudy/config/JobRunner.java
+++ src/main/java/com/minchul/springbatchstudy/config/JobRunner.java
@@ -1,5 +1,6 @@
 package com.minchul.springbatchstudy.config;
 
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -19,6 +20,9 @@ public class JobRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         jobLauncher.run(job, new JobParametersBuilder()
             .addString("name", "user1")
+            .addLong("seq", 2L)
+            .addDate("Date", new Date())
+            .addDouble("height", 182.5)
             .toJobParameters());
     }
 }
