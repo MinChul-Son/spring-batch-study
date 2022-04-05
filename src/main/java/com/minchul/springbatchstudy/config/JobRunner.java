2e82787564a488dad165fa8500e0b5af722cf843
MinChul-Son <smc5236@gmail.com>
2022-04-05 19:57:42 +0900

    ✨ feat: JobLauncher를 통해 JobInstance를 직접 생성

---
+++ src/main/java/com/minchul/springbatchstudy/config/JobRunner.java
@@ -0,0 +1,24 @@
+package com.minchul.springbatchstudy.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        jobLauncher.run(job, new JobParametersBuilder()
+            .addString("name", "user1")
+            .toJobParameters());
+    }
+}
