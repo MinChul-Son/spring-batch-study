74d8270d9214f5d121d30bfbb948e0ac0d1f7253
MinChul-Son <smc5236@gmail.com>
2022-04-10 18:50:25 +0900

    ✨ feat: BatchConfigurer 설정 커스터마이징
    - 배치가 초기화되는 시점에 JobRepository 설정을 커스터마이징 가능

---
+++ src/main/java/com/minchul/springbatchstudy/config/CustomBatchConfig.java
@@ -0,0 +1,43 @@
+package com.minchul.springbatchstudy.config;
+
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+//@Configuration
+public class CustomBatchConfig extends BasicBatchConfigurer {
+
+    private final DataSource dataSource;
+
+    /**
+     * Create a new {@link BasicBatchConfigurer} instance.
+     *
+     * @param properties                    the batch properties
+     * @param dataSource                    the underlying data source
+     * @param transactionManagerCustomizers transaction manager customizers (or {@code null})
+     */
+    protected CustomBatchConfig(
+        BatchProperties properties,
+        DataSource dataSource,
+        TransactionManagerCustomizers transactionManagerCustomizers) {
+        super(properties, dataSource, transactionManagerCustomizers);
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected JobRepository createJobRepository() throws Exception {
+        JobRepositoryFactoryBean factoryBean = new JobRepositoryFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTransactionManager(getTransactionManager());
+        factoryBean.setIsolationLevelForCreate("ISOLATION_READ_COMMITTED");
+        factoryBean.setTablePrefix("SYSTEM_");
+
+        return factoryBean.getObject();
+    }
+}
