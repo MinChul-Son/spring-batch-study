4f7c3ad901677d0d215234b3717c01c4da627e35
MinChul-Son <smc5236@gmail.com>
2022-04-10 19:46:46 +0900

    ✨ feat: 컨트롤러 구현
    - 요청이 오면 SimpleJobLauncher의 TaskExecutor를 AsyncTaskExecutor로 변경하여 작업을 수행

---
+++ src/main/java/com/minchul/springbatchstudy/controller/JobLauncherController.java
@@ -0,0 +1,43 @@
+package com.minchul.springbatchstudy.controller;
+
+import com.minchul.springbatchstudy.domain.Member;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JobLauncherController {
+
+    private final Job job;
+    private final JobLauncher jobLauncher;
+    private final BasicBatchConfigurer basicBatchConfigurer;
+
+    @PostMapping("/batch")
+    public String launch(@RequestBody Member member)
+        throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addString("name", member.name())
+            .addDate("date", new Date())
+            .toJobParameters();
+        SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        this.jobLauncher.run(job, jobParameters);
+
+        return "Batch Completed";
+    }
+}
