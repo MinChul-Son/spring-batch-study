7481bc7cd0d9b205f20fb7c8609b6170b843d790
MinChul-Son <smc5236@gmail.com>
2022-05-02 13:47:02 +0900

    ✨ feat: DefaultJobParametersValidator 사용

--- src/main/java/com/minchul/springbatchstudy/config/ValidatorConfig.java
+++ src/main/java/com/minchul/springbatchstudy/config/ValidatorConfig.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -25,7 +26,8 @@ public class ValidatorConfig {
         return jobBuilderFactory.get("batchJob")
                                 .start(step1())
                                 .next(step2())
-            .validator(new CustomJobParameterValidator())
+//            .validator(new CustomJobParameterValidator())
+            .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
                                 .build();
     }
 
