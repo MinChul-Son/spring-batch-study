65da0d0718062871e84045ce23f61aa9bc2927ae
MinChul-Son <smc5236@gmail.com>
2022-05-02 11:03:47 +0900

    ✨ feat: SimpleJob API 살펴보기

---
+++ src/main/java/com/minchul/springbatchstudy/config/SimpleJobConfig.java
@@ -0,0 +1,59 @@
+package com.minchul.springbatchstudy.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SimpleJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                                .start(step1())
+                                .next(step2())
+            .incrementer(new RunIdIncrementer())
+            .validator(parameters -> {
+
+            })
+                                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step1");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+                                     contribution.setExitStatus(ExitStatus.STOPPED);
+                                     log.info("Hello Spring Batch - step2");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+}
