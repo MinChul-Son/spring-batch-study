7e77b3119e0917b2b540a744dccb6f4ab685eff1
MinChul-Son <smc5236@gmail.com>
2022-05-22 22:58:23 +0900

    🚀 feat: 중첩 flow 예제

--- src/main/java/com/minchul/springbatchstudy/config/SimpleFlowConfig.java
+++ src/main/java/com/minchul/springbatchstudy/config/SimpleFlowConfig.java
@@ -23,17 +23,40 @@ public class SimpleFlowConfig {
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
-                                .start(flow())
-                                .next(step3())
+                                .start(flow1())
+                                .on("COMPLETED").to(flow2())
+                                .from(flow1())
+                                .on("FAILED").to(flow3())
                                 .end()
                                 .build();
     }
 
     @Bean
-    public Flow flow() {
-        FlowBuilder<Flow> builder = new FlowBuilder<>("flow");
+    public Flow flow1() {
+        FlowBuilder<Flow> builder = new FlowBuilder<>("flow1");
         builder.start(step1())
                .next(step2())
+               .end();
+
+        return builder.build();
+    }
+
+    @Bean
+    public Flow flow2() {
+        FlowBuilder<Flow> builder = new FlowBuilder<>("flow2");
+        builder.start(flow3())
+               .next(step5())
+               .next(step6())
+               .end();
+
+        return builder.build();
+    }
+
+    @Bean
+    public Flow flow3() {
+        FlowBuilder<Flow> builder = new FlowBuilder<>("flow3");
+        builder.start(step3())
+               .next(step4())
                .end();
 
         return builder.build();
@@ -62,6 +85,33 @@ public class SimpleFlowConfig {
         return stepBuilderFactory.get("step3")
                                  .tasklet((contribution, chunkContext) -> {
                                      log.info("Hello Spring Batch - step3");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step4");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step5");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step6() {
+        return stepBuilderFactory.get("step6")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step6");
                                      return RepeatStatus.FINISHED;
                                  }).build();
     }
