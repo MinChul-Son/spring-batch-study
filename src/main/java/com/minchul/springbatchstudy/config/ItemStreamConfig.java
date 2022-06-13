a4ea16568f881b6e4310d4e4dfdd672f0fe9f3db
MinChul-Son <smc5236@gmail.com>
2022-06-13 22:31:39 +0900

    ✨ feat: ItemStreamReader, ItemStreamWriter 실습

---
+++ src/main/java/com/minchul/springbatchstudy/config/ItemStreamConfig.java
@@ -0,0 +1,70 @@
+package com.minchul.springbatchstudy.config;
+
+import com.minchul.springbatchstudy.CustomItemProcessor;
+import com.minchul.springbatchstudy.CustomItemReader;
+import com.minchul.springbatchstudy.CustomItemStreamReader;
+import com.minchul.springbatchstudy.CustomItemStreamWriter;
+import com.minchul.springbatchstudy.CustomItemWriter;
+import com.minchul.springbatchstudy.domain.Member;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ItemStreamConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                                .start(step1())
+                                .next(step2())
+                                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .<String, String>chunk(5)
+                                 .reader(itemReader())
+                                 .writer(itemWriter())
+                                 .build();
+    }
+
+    private ItemWriter<? super String> itemWriter() {
+        return new CustomItemStreamWriter();
+    }
+
+    @Bean
+    public CustomItemStreamReader itemReader() {
+        List<String> items = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            items.add(String.valueOf(i));
+        }
+        return new CustomItemStreamReader(items);
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     log.info("Hello Spring Batch - step2");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+}
