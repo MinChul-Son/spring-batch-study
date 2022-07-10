be3f6c80524174ce4da3eeaba36770b315ced381
MinChul-Son <smc5236@gmail.com>
2022-07-10 18:20:09 +0900

    ✨ feat: JsonItemReader 실습(JacksonJsonObjectReader)

---
+++ src/main/java/com/minchul/springbatchstudy/config/JsonConfig.java
@@ -0,0 +1,58 @@
+package com.minchul.springbatchstudy.config;
+
+import com.minchul.springbatchstudy.domain.Customer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class JsonConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                                .incrementer(new RunIdIncrementer())
+                                .start(step1())
+                                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .<Customer, Customer>chunk(3)
+                                 .reader(customItemReader())
+                                 .writer(items -> {
+                                     log.info("items = {}", items);
+                                 })
+                                 .build();
+    }
+
+    @Bean
+    public ItemReader<? extends Customer> customItemReader() {
+        return new JsonItemReaderBuilder<Customer>()
+            .name("jsonReader")
+            .resource(new ClassPathResource("/customer.json"))
+            .jsonObjectReader(new JacksonJsonObjectReader<>(Customer.class))
+            .build();
+    }
+}
