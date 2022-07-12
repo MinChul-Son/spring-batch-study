d2a1ad43ec51cb371ddc713b015272f01fc81cbb
MinChul-Son <smc5236@gmail.com>
2022-07-12 23:24:50 +0900

    ✨ feat: DB 데이터와 매핑될 샘플 도메인 객체 생성

---
+++ src/main/java/com/minchul/springbatchstudy/domain/Person.java
@@ -0,0 +1,12 @@
+package com.minchul.springbatchstudy.domain;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+
+}
