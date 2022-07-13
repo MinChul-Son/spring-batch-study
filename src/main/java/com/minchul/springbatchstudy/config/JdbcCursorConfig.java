210a442acbd2d05f0685ad81f9fc29545f416ac5
MinChul-Son <smc5236@gmail.com>
2022-07-13 22:53:35 +0900

    🚀 refactor: 이전 코드 주석 처리

--- src/main/java/com/minchul/springbatchstudy/config/JdbcCursorConfig.java
+++ src/main/java/com/minchul/springbatchstudy/config/JdbcCursorConfig.java
@@ -14,7 +14,7 @@ import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuild
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class JdbcCursorConfig {
