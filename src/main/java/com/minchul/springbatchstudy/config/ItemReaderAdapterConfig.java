3f2ec5cc0aeba03f84d6cbce6d3b15b94849d654
MinChul-Son <smc5236@gmail.com>
2022-07-16 19:50:12 +0900

    🚀 refactor: 이전 코드 주석 처리

--- src/main/java/com/minchul/springbatchstudy/config/ItemReaderAdapterConfig.java
+++ src/main/java/com/minchul/springbatchstudy/config/ItemReaderAdapterConfig.java
@@ -12,7 +12,7 @@ import org.springframework.batch.item.adapter.ItemReaderAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class ItemReaderAdapterConfig {
