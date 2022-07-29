f73ffc01a43c9a577e22dc73ecf29762ccfe1d60
MinChul-Son <smc5236@gmail.com>
2022-07-29 22:20:25 +0900

    ✨ feat: ItemWriterAdapter 실습

---
+++ src/main/java/com/minchul/springbatchstudy/config/ItemWriterAdapterConfig.java
@@ -0,0 +1,49 @@
+package com.minchul.springbatchstudy.config;
+
+import com.minchul.springbatchstudy.service.CustomService;
+import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.adapter.ItemWriterAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ItemWriterAdapterConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+                                .incrementer(new RunIdIncrementer())
+                                .start(step1())
+                                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .<String, String>chunk(10)
+                                 .reader(() -> String.valueOf(IntStream.rangeClosed(1, 10)))
+                                 .writer(customItemWriter())
+                                 .build();
+    }
+
+    @Bean
+    public ItemWriter<? super String> customItemWriter() {
+        ItemWriterAdapter<String> writer = new ItemWriterAdapter<>();
+        writer.setTargetObject(new CustomService());
+        writer.setTargetMethod("doSomething");
+        return writer;
+    }
+}
