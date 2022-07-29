0d9f40f8d312283721a009dc061b4b4d62fb9a5b
MinChul-Son <smc5236@gmail.com>
2022-07-29 21:59:01 +0900

    🚀 refactor: 이전코드 주석

--- src/main/java/com/minchul/springbatchstudy/config/JdbcBatchConfig.java
+++ src/main/java/com/minchul/springbatchstudy/config/JdbcBatchConfig.java
@@ -17,7 +17,7 @@ import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class JdbcBatchConfig {
